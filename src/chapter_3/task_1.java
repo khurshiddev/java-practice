ca7460a02b1087bf01c7ac7f7aa4058f3e64070e
khurshiddev <umaribnxattob8008@gmail.com>
2024-12-14 13:10:54 +0900

    feat: Add new project

---
+++ src/chapter_3/task_1.java
@@ -0,0 +1,12 @@
+package chapter_3;
+
+public class task_1 {
+    public static void main(String[] args) {
+        String s1 = "salom hammaga, salom do'stlar";
+        String s2 = "Gita";
+
+        s2 = s1.replace("salom",s2);
+        System.out.println(s2);
+
+    }
+}
