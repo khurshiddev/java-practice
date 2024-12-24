d89b289441c34a61a6622e9e8b5aba260e6fc197
khurshiddev <umaribnxattob8008@gmail.com>
2024-12-24 21:26:31 +0900

    feat: add new project

---
+++ src/chapter_11/GetIndexTask.java
@@ -0,0 +1,28 @@
+package chapter_11;
+
+public class GetIndexTask {
+    public static void main(String[] args) {
+        A a1 = new A();
+        A a2 = new A();
+        A a3 = new A();
+        A a4 = new A();
+
+        System.out.println(a2.getIndex());
+        System.out.println(a1.getIndex());
+        System.out.println(a2.getIndex());
+        System.out.println(a3.getIndex());
+        System.out.println(a4.getIndex());
+    }
+}
+
+class A{
+   private int incCount;
+    static int counter;
+    public A(){
+        incCount = ++counter;
+    }
+    public int getIndex(){
+        return incCount;
+    }
+
+}
