5924b64402a6a08f835c8f49224a2a3c4ee4a7af
khurshiddev <umaribnxattob8008@gmail.com>
2025-01-05 21:40:03 +0900

    feat: add new project

---
+++ src/chapter_8/task_1/Ingredients.java
@@ -0,0 +1,11 @@
+package chapter_8.task_1;
+
+public class Ingredients {
+    String name;
+    int count;
+
+    public Ingredients(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+}
