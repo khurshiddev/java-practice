1d2ba243f4c1b4fdf89077d218cc863f6dfc733b
khurshiddev <umaribnxattob8008@gmail.com>
2025-01-15 14:35:53 +0900

    feat: add new project

--- src/chapter_16/WalletTaskl.java
+++ src/chapter_16/WalletTaskl.java
@@ -6,55 +6,36 @@ import java.util.Objects;
 
 public class WalletTaskl {
     public static void main(String[] args) {
-        Card card1 = new Card("User1", "1234", 1000);
-//        wallet.addCard(card1);
-//
-//        wallet.spendMoneyFromCard(card1, 700);
-//        assertEquals(300, card1.getAmount());
-
-
-        Card car1 = new Card("Abdulloh", "123443454122", 1000);
-        Card car2 = new Card("Abduqodir", "122243454122", 120000);
-        Card car3 = new Card("Behzod", "124443454122", 180000);
-        Card car4 = new Card("Oybek", "125543454122", 250000);
-        Card car5 = new Card("Abdulloh", "123443454122", 500000);
-        Wallet wallet = new Wallet(30000);
-        wallet.setLogListener(System.out::println);
+        Card card1 = new Card("Abdulloh", "123443454122", 1000);
+        Card card2 = new Card("Abduqodir", "122243454122", 120000);
+        Card card3 = new Card("Behzod", "124443454122", 180000);
+        Card card4 = new Card("Oybek", "125543454122", 250000);
+        Card card5 = new Card("Abdulloh", "123443454122", 500000);
+        Wallet wallet = new Wallet(15000);
         wallet.setCardListener(System.out::println);
-        List<Card> cards = new ArrayList<>();
-        cards.add(car1);
-        wallet.spendMoneyFromCard(card1, 700);
-//        System.out.println(wallet.getTotalBalance());
-        System.out.println(card1.getAmount());
-        cards.add(car2);
-        cards.add(car3);
-        cards.add(car4);
-        cards.add(car5);
-        wallet.addBalance(88000);
-        wallet.addCard(car1);
-        wallet.addCard(car2);
-        wallet.addCard(car3);
-        wallet.addCard(car4);
-        wallet.addCard(car5);
+        wallet.setLogListener(System.out::println);
+
+        wallet.addCard(card1);
+        wallet.addCard(card2);
+        wallet.addBalance(20000);
+        wallet.addCard(card3);
+
+        wallet.addCard(card1);
         wallet.getAllCards();
-        wallet.addCards(cards);
-        wallet.spendMoney(200000);
-
         System.out.println(wallet.getTotalBalance());
-        wallet.spendMoneyFromCard(car3, 100000);
-        wallet.spendMoneyFromCard(car1, 650000);
-
-
+        wallet.spendMoney(650000);
+        wallet.spendMoneyFromCard(card1, 150000);
+        wallet.spendMoneyFromCard(card4, 80000);
     }
 }
 
 class Card {
-    private String userName, cardName;
+    private String username, cardNumber;
     private int amount;
 
-    public Card(String userName, String cardName, int amount) {
-        this.userName = userName;
-        this.cardName = cardName;
+    public Card(String username, String cardNumber, int amount) {
+        this.username = username;
+        this.cardNumber = cardNumber;
         this.amount = amount;
     }
 
@@ -62,28 +43,28 @@ class Card {
         return amount;
     }
 
-    public void spendMoney(int value) {
-        this.amount -= value;
+    public void spendMoney(int amount) {
+        this.amount -= amount;
     }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Card card = (Card) o;
-        return amount == card.amount && Objects.equals(userName, card.userName) && Objects.equals(cardName, card.cardName);
+        return amount == card.amount && Objects.equals(username, card.username) && Objects.equals(cardNumber, card.cardNumber);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(userName, cardName, amount);
+        return Objects.hash(username, cardNumber, amount);
     }
 
     @Override
     public String toString() {
         return "Card{" +
-                "userName = '" + userName + '\'' +
-                ", cardName = '" + cardName + '\'' +
-                ", amount = " + amount +
+                "username='" + username + '\'' +
+                ", cardNumber='" + cardNumber + '\'' +
+                ", amount=" + amount +
                 '}';
     }
 }
@@ -109,6 +90,30 @@ class Wallet {
         cards = new ArrayList<>();
     }
 
+    void addCard(Card card) {
+        if (checkCard(card) != -1) {
+            logListener.sendMessage("bu avval qoshilgan ");
+
+        } else {
+            logListener.sendMessage("karta qoshildi ");
+            cards.add(card);
+        }
+    }
+
+    public void addCards(List<Card> cards) {
+        for (Card card : cards) {
+            if (checkCard(card) != -1) {
+                cardListener.showCardData(card);
+                return;
+            }
+            cards.add(card);
+        }
+    }
+
+    public void addBalance(int addBalance) {
+        balance += addBalance;
+    }
+
     public void setCardListener(ShowCardInterface cardListener) {
         this.cardListener = cardListener;
     }
@@ -117,64 +122,12 @@ class Wallet {
         this.logListener = logListener;
     }
 
-    public void addCard(Card card) {
-        if (check(card) != -1) {
-            logListener.sendMessage("bu karta avval qo'shilgan");
-        } else {
-            logListener.sendMessage("carta qo'shildi");
-            cards.add(card);
-        }
-    }
-
-    public void addCards(List<Card> cards) {
-        for (Card card : cards) {
-            if (check(card) != -1) {
-                cardListener.showCardData(card);
-                return;
-            }
-            cards.add(card);
-        }
-    }
-
-    public int addBalance(int addBalance) {
-        return balance += addBalance;
-    }
-
     public int getTotalBalance() {
-        int total = balance;
+        int totalBalance = balance;
         for (Card card : cards) {
-            total += card.getAmount();
+            totalBalance += card.getAmount();
         }
-        return total;
-    }
-
-    public void getAllCards() {
-        for (Card card : cards) {
-            cardListener.showCardData(card);
-        }
-    }
-
-    public void spendMoney(int money) {
-        if (balance >= money) {
-            balance -= money;
-        } else {
-            logListener.sendMessage(money - balance + " = shuncha pul yetmadi");
-        }
-    }
-
-    public void spendMoneyFromCard(Card card, int spendMoney) {
-        int checkIndex = check(card);
-        if (checkIndex != -1) {
-            logListener.sendMessage("bu carta hamyonda yo'q");
-        } else {
-            if (card.getAmount() >= spendMoney) {
-                card.spendMoney(spendMoney);
-            } else {
-                logListener.sendMessage(spendMoney - card.getAmount() + " = shuncha miqdorda pul yetmayapti");
-            }
-
-        }
-
+        return totalBalance;
     }
 
     @Override
@@ -184,7 +137,37 @@ class Wallet {
                 '}';
     }
 
-    private int check(Card card) {
+    public void getAllCards() {
+
+        for (Card card : cards) {
+            cardListener.showCardData(card);
+        }
+    }
+
+    public void spendMoney(int money) {
+        if (balance >= money) {
+            balance -= money;
+        } else {
+            logListener.sendMessage(money - balance + " miqdorda naqd pul yetmaydi");
+        }
+
+    }
+
+    public void spendMoneyFromCard(Card card, int spendMoney) {
+        int index = checkCard(card);
+        if (index == -1) {
+            logListener.sendMessage("bu karta hamyonda yoq");
+        } else {
+            if (card.getAmount() >= spendMoney) {
+                card.spendMoney(spendMoney);
+            } else {
+                logListener.sendMessage(spendMoney - card.getAmount() + " miqdorda naqd pul yetmaydi");
+            }
+        }
+    }
+
+    private int checkCard(Card card) {
         return cards.indexOf(card);
     }
 }
+
